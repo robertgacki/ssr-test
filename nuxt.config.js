4869a1ae5fc2aef3dfb7efaa599256cd2e294169
robertgacki <r.gacki@gmail.com>
2020-09-07 14:52:42 +0200

    meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,5 +5,18 @@ export default {
     router: {
         base: '/catalog/'
     },
-    modules: ['@nuxt/http']
+    modules: ['@nuxt/http'],
+    head: {
+        title: 'Fitatu the best calories calculator',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'my website description'
+            }
+        ],
+        link: [{ rel: 'icon', type: 'image/png', href: 'favicon.png' }]
+    }
 }
